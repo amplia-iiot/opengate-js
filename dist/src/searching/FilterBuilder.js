f9e0e746f1e83c8f77748e16b4f330c50d28caf5
Victor Gutierrez <victor.gutierrez@amplia.es>
2017-09-26 15:19:31 +0200

    Control to not allow add 'and' & 'or'  into the filter

--- dist/src/searching/FilterBuilder.js
+++ dist/src/searching/FilterBuilder.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
-	value: true
+    value: true
 });
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
@@ -16,58 +16,68 @@ var _utilExpression2 = _interopRequireDefault(_utilExpression);
 
 var FilterBuilder = (function () {
 
-	/**
-  * @example
-  * var fb = ogapi.newFilterBuilder()
-  */
+    /**
+     * @example
+     * var fb = ogapi.newFilterBuilder()
+     */
 
-	function FilterBuilder() {
-		_classCallCheck(this, FilterBuilder);
+    function FilterBuilder() {
+        _classCallCheck(this, FilterBuilder);
 
-		this._filterTemplate = { filter: {} };
-	}
+        this._filterTemplate = { filter: {} };
+    }
 
-	/**
-  * @example
-  * fb.or(Ex.like("collected.serialNumber", "SN"), Ex.eq("entityId", "e64ccd08-e302-4b65-b19d-e38eeb7b2d25"))
-  * @param {[Expression]} args - The parameters will be operators of the class Expression
-  */
+    /**
+     * @example
+     * fb.or(Ex.like("collected.serialNumber", "SN"), Ex.eq("entityId", "e64ccd08-e302-4b65-b19d-e38eeb7b2d25"))
+     * @param {[Expression]} args - The parameters will be operators of the class Expression
+     */
 
-	_createClass(FilterBuilder, [{
-		key: 'or',
-		value: function or() {
-			if (typeof this._filterTemplate.filter.or === "undefined") this._filterTemplate.filter.or = [];
+    _createClass(FilterBuilder, [{
+        key: 'or',
+        value: function or() {
+            if (typeof this._filterTemplate.filter.or === "undefined") this._filterTemplate.filter.or = [];
 
-			for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-				args[_key] = arguments[_key];
-			}
+            if (typeof this._filterTemplate.filter.and !== "undefined") {
+                console.warn('The filter not allow both elements [and,or], the item "and" are going to be removed.');
+                delete this._filterTemplate.filter.and;
+            }
 
-			for (var i = 0; i < args.length; i++) this._filterTemplate.filter.or.push(args[i]);
+            for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+                args[_key] = arguments[_key];
+            }
 
-			return this;
-		}
+            for (var i = 0; i < args.length; i++) this._filterTemplate.filter.or.push(args[i]);
 
-		/**
-   * @example
-   * fb.and(Ex.like("collected.serialNumber", "SN"), Ex.eq("entityId", "e64ccd08-e302-4b65-b19d-e38eeb7b2d25"))
-   * @param {[Expression]} args - The parameters will be operators of the class Expression
-   */
-	}, {
-		key: 'and',
-		value: function and() {
-			if (typeof this._filterTemplate.filter.and === "undefined") this._filterTemplate.filter.and = [];
+            return this;
+        }
 
-			for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
-				args[_key2] = arguments[_key2];
-			}
+        /**
+         * @example
+         * fb.and(Ex.like("collected.serialNumber", "SN"), Ex.eq("entityId", "e64ccd08-e302-4b65-b19d-e38eeb7b2d25"))
+         * @param {[Expression]} args - The parameters will be operators of the class Expression
+         */
+    }, {
+        key: 'and',
+        value: function and() {
+            if (typeof this._filterTemplate.filter.and === "undefined") this._filterTemplate.filter.and = [];
 
-			for (var i = 0; i < args.length; i++) this._filterTemplate.filter.and.push(args[i]);
+            if (typeof this._filterTemplate.filter.or !== "undefined") {
+                console.warn('The filter not allow both elements [and,or], the item "or" are going to be removed.');
+                delete this._filterTemplate.filter.or;
+            }
 
-			return this;
-		}
-	}]);
+            for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+                args[_key2] = arguments[_key2];
+            }
 
-	return FilterBuilder;
+            for (var i = 0; i < args.length; i++) this._filterTemplate.filter.and.push(args[i]);
+
+            return this;
+        }
+    }]);
+
+    return FilterBuilder;
 })();
 
 exports['default'] = FilterBuilder;
--- src/searching/FilterBuilder.js
+++ src/searching/FilterBuilder.js
@@ -4,43 +4,52 @@ import Ex from './../util/Expression'
 
 export default class FilterBuilder {
 
-/**
- * @example
- * var fb = ogapi.newFilterBuilder()
- */
-	constructor(){
-		this._filterTemplate = {filter : {}}
-	}
+    /**
+     * @example
+     * var fb = ogapi.newFilterBuilder()
+     */
+    constructor() {
+        this._filterTemplate = { filter: {} }
+    }
 
-/**
- * @example
- * fb.or(Ex.like("collected.serialNumber", "SN"), Ex.eq("entityId", "e64ccd08-e302-4b65-b19d-e38eeb7b2d25"))
- * @param {[Expression]} args - The parameters will be operators of the class Expression
- */
-	or(...args){
-		if (typeof this._filterTemplate.filter.or === "undefined")
-			this._filterTemplate.filter.or = [];
+    /**
+     * @example
+     * fb.or(Ex.like("collected.serialNumber", "SN"), Ex.eq("entityId", "e64ccd08-e302-4b65-b19d-e38eeb7b2d25"))
+     * @param {[Expression]} args - The parameters will be operators of the class Expression
+     */
+    or(...args) {
+        if (typeof this._filterTemplate.filter.or === "undefined")
+            this._filterTemplate.filter.or = [];
 
-		for (var i = 0; i < args.length; i++)
-			this._filterTemplate.filter.or.push(args[i])
+        if (typeof this._filterTemplate.filter.and !== "undefined") {
+            console.warn('The filter not allow both elements [and,or], the item "and" are going to be removed.');
+            delete this._filterTemplate.filter.and;
+        }
 
-		return this;
-	}
+        for (var i = 0; i < args.length; i++)
+            this._filterTemplate.filter.or.push(args[i])
 
-/**
- * @example
- * fb.and(Ex.like("collected.serialNumber", "SN"), Ex.eq("entityId", "e64ccd08-e302-4b65-b19d-e38eeb7b2d25"))
- * @param {[Expression]} args - The parameters will be operators of the class Expression
- */
-	and(...args){		
-		if (typeof this._filterTemplate.filter.and === "undefined")
-			this._filterTemplate.filter.and = [];
+        return this;
+    }
 
-		for (var i = 0; i < args.length; i++)
-			this._filterTemplate.filter.and.push(args[i])
-		
-		return this;
-	}
+    /**
+     * @example
+     * fb.and(Ex.like("collected.serialNumber", "SN"), Ex.eq("entityId", "e64ccd08-e302-4b65-b19d-e38eeb7b2d25"))
+     * @param {[Expression]} args - The parameters will be operators of the class Expression
+     */
+    and(...args) {
+        if (typeof this._filterTemplate.filter.and === "undefined")
+            this._filterTemplate.filter.and = [];
+
+        if (typeof this._filterTemplate.filter.or !== "undefined") {
+            console.warn('The filter not allow both elements [and,or], the item "or" are going to be removed.');
+            delete this._filterTemplate.filter.or;
+        }
+
+        for (var i = 0; i < args.length; i++)
+            this._filterTemplate.filter.and.push(args[i])
+
+        return this;
+    }
 
 }
-
