fcf49a0b53218a3ee315083d8ad09542afa6e076
kmlamplia <katherin.moscoso@amplia.es>
2018-03-09 10:42:50 +0100

    Cambio Configuracion

--- features/support/world.js
+++ features/support/world.js
@@ -14,6 +14,7 @@ searchingModel = require('./searching_model');
 function World() {
     this.test_url_north = process.env.API_NORTH_INTERNAL || 'SECURE_URL_NORTH';
     this.test_url_south = process.env.API_SOUTH_INTERNAL || 'REQUIRE-URL_SOUTH';
+    this.apikey = process.env.API_KEY || 'REQUIRE-API_KEY';
     chai.use(chai_as_promised);
     this.expect = chai.expect;
 
@@ -24,7 +25,7 @@ function World() {
     this.findDeleteModel = findDeleteModel;
     this.searchingModel = searchingModel;
 
-    this.model_match = function (model) {
+    this.model_match = function(model) {
         return {
             'create': this.findCreateModel,
             'update': this.findCreateModel,
@@ -40,13 +41,13 @@ function World() {
 
     _this.___lastPercent = 0;
 
-    this.uploadProgress = function (arg) {
+    this.uploadProgress = function(arg) {
         if (arg.loaded && arg.total) {
             _this.___lastPercent = (arg.loaded * 100) / arg.total;
         }
     };
 }
 
-module.exports = function () {
+module.exports = function() {
     this.World = World;
 };
