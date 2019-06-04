45f8ca886dc148337f34901cfd682e0f1692bf2c
kmlamplia <katherin.moscoso@amplia.es>
2019-06-04 17:21:08 +0200

    OUW-2163

--- src/areas/Areas.js
+++ src/areas/Areas.js
@@ -1,10 +1,8 @@
 'use strict';
 
 import { TYPE_ENUM } from './TYPE_ENUM';
-
-import q from 'q';
 import BaseProvision from '../provision/BaseProvision';
-
+import checkType from '../util/formats/check_types'
 /**
  * This is a base object that contains all you can do about Areas.
  */
@@ -29,7 +27,7 @@ export default class Areas extends BaseProvision {
      * @return {Areas}
      */
     withOrganization(organization) {
-        this._checkString(organization, 50, 'organization');
+        checkType._checkString(organization, 50, 'organization');
         this._organization = organization;
         return this;
     }
@@ -40,7 +38,7 @@ export default class Areas extends BaseProvision {
      * @return {Areas}
      */
     withIdentifier(identifier) {
-        this._checkString(identifier, 50, 'identifier');
+        checkType._checkString(identifier, 50, 'identifier');
         this._identifier = identifier;
         return this;
     }
@@ -51,7 +49,7 @@ export default class Areas extends BaseProvision {
      * @return {Areas}
      */
     withName(name) {
-        this._checkString(name, 50, 'name');
+        checkType._checkString(name, 50, 'name');
         this._name = name;
         return this;
     }
@@ -62,10 +60,23 @@ export default class Areas extends BaseProvision {
      * @return {Areas}
      */
     withDescription(description) {
-        this._checkString(description, 50, 'description');
+        checkType._checkString(description, 50, 'description');
         this._description = description;
         return this;
     }
+
+
+    /**
+     * Set the order attribute
+     * @param {number} order - required field
+     * @return {Areas}
+     */
+    withOrder(order) {
+        checkType._checkNumber(order, 'order');
+        this._order = order;
+        return this;
+    }
+
 
     /**
      * Set the geometry attribute
@@ -74,8 +85,8 @@ export default class Areas extends BaseProvision {
      * @return {Areas}
      */
     withGeometry(type, coordinates) {
-        this._checkType(type);
-        this._checkArray(coordinates, 'coordinates');
+        checkType._checkType(type, TYPE_ENUM);
+        checkType._checkArray(coordinates, 'coordinates');
         this._type = type;
         this._coordinates = coordinates;
         return this;
@@ -87,38 +98,11 @@ export default class Areas extends BaseProvision {
      * @return {Areas}
      */
     withEntities(entities) {
-        this._checkArray(entities, 'entities');
+        checkType._checkArray(entities, 'entities');
         this._entities = entities;
         return this;
     }
 
-    /* fields validations */
-    _checkType(type) {
-        let not_found = [];
-        let found = TYPE_ENUM.find(function(type) {
-            return type == this;
-        }, type);
-
-        if (typeof found === "undefined") {
-            not_found.push(type);
-        }
-        if (not_found.length !== 0) {
-            throw new Error({ message: "OGAPI_NOT_ALLOWED_PARAMETER", parameter: JSON.stringify(not_found), allowed: JSON.stringify(TYPE_ENUM) });
-        }
-        return type;
-    }
-
-    _checkString(parameter, length, name) {
-        if (typeof parameter !== 'string' || parameter.length > length) {
-            throw new Error([{ message: 'OGAPI_STRING_PARAMETER', parameter: name }, { message: 'OGAPI_MAX_LENGTH', parameter: length }]);
-        }
-    }
-
-    _checkArray(parameter, name) {
-        if (!Array.isArray(parameter) || parameter.length === 0) {
-            throw new Error({ message: 'OGAPI_ARRAY_PARAMETER', parameter: name });
-        }
-    }
 
     _composeElement() {
         this._resource = 'provision/organizations/' + this._organization + '/areas/';
@@ -126,6 +110,8 @@ export default class Areas extends BaseProvision {
             identifier: this._identifier,
             name: this._name,
             description: this._description,
+            order: this._order,
+
             geometry: {
                 type: this._type,
                 coordinates: this._coordinates
