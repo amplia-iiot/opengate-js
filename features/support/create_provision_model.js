038401c061f3b399978bc589632d3e59f3c824df
Manuel <manuel.haro@amplia.es>
2021-04-20 16:52:56 +0200

    OUW-1087-2
    OUW-1087-2

--- features/support/create_provision_model.js
+++ features/support/create_provision_model.js
@@ -370,15 +370,16 @@ module.exports = {
                 'organization': 'withOrganization',
                 'description': 'withDescription',
                 'geometry': 'withGeometry',
+                'color': 'withColor',
                 'entities': 'withEntities'
             }
 
-        } [setter];
+        }[setter];
     },
     getter_response: function (getter) {
         return {
             'user email': '/email',
             'organization name': '/name'
-        } [getter];
+        }[getter];
     }
 };
