ea445f5a5b21740c3b9b1f34efad9028db95e1e0
Chema Perez <josemaria.perez@amplia.es>
2022-09-16 12:38:11 +0200

    OUW-3756

---
+++ src/manufacturers/Manufacturer.js
@@ -0,0 +1,150 @@
+'use strict';
+
+import BaseProvision from '../provision/BaseProvision';
+import ManufacturerMedia from './ManufacturerMedia';
+import Model from './Model';
+
+/**
+ * This is a base object that contains all you can do about Manufacturers.
+ */
+export default class Manufacturers extends BaseProvision {
+
+    /**     
+     * @param {InternalOpenGateAPI} Reference to the API object.
+     */
+    constructor(ogapi) {
+        super(ogapi, "/manufacturers", undefined, ['identifier', 'name']);
+    }
+
+    /**
+     * Set the identifier attribute
+     * @param {string} id - required field
+     * @return {Manufacturers}
+     */
+    withIdentifier(id) {
+        if (typeof id !== 'string' || id.length > 50)
+            throw new Error("OGAPI_STRING_PARAMETER_MAX_LENGTH_50");
+        this._identifier = id;
+        return this;
+    }
+    
+    /**
+     * Set the name attribute
+     * @param {string} name - required field
+     * @return {Manufacturers}
+     */
+    withName(name) {
+        if (typeof name !== 'string')
+            throw new Error("OGAPI_STRING_PARAMETER");
+        this._name = name;
+        return this;
+    }
+
+    /**
+     * Set the description attribute
+     * @param {string} description - required field
+     * @return {Manufacturers}
+     */
+    withDescription(description) {
+        if (typeof description !== 'string')
+            throw new Error("OGAPI_STRING_PARAMETER");
+        this._description = description;
+        return this;
+    }
+
+    /**
+     * Set the notes attribute
+     * @param {string} notes 
+     * @return {Manufacturers}
+     */
+    withNotes(notes) {
+        if (typeof notes !== 'string')
+           throw new Error("OGAPI_STRING_PARAMETER");
+        this._notes = notes;
+        return this;
+    }
+
+    /**
+     * Set the url attribute
+     * @param {string} url 
+     * @return {Manufacturers}
+     */
+    withUrl(url) {
+        if (typeof url !== 'string')
+           throw new Error("OGAPI_STRING_PARAMETER");
+        this._manufUrl = url;
+        return this;
+    }
+
+    /**
+     * Set the fax attribute
+     * @param {string} fax 
+     * @return {Manufacturers}
+     */
+    withFax(fax) {
+        if (typeof fax !== 'string')
+           throw new Error("OGAPI_STRING_PARAMETER");
+        this._fax = fax;
+        return this;
+    }
+
+    /**
+     * Set the telephone attribute
+     * @param {string} telephone 
+     * @return {Manufacturers}
+     */
+    withTelephone(telephone) {
+        if (typeof telephone !== 'string')
+           throw new Error("OGAPI_STRING_PARAMETER");
+        this._telephone = telephone;
+        return this;
+    }
+
+    /**
+     * Set the address attribute
+     * @param {string} address 
+     * @return {Manufacturers}
+     */
+    withAddress(address) {
+        if (typeof address !== 'string')
+           throw new Error("OGAPI_STRING_PARAMETER");
+        this._address = address;
+        return this;
+    }
+
+    mediaBuilder() {
+        if (!this._identifier)
+            throw new Error("Required manufacturer identifier");
+        return new ManufacturerMedia(this._ogapi, this._identifier)
+    }
+
+    modelBuilder() {
+        if (!this._identifier || !this._name)
+            throw new Error("Required manufacturer identifier and name");
+        return new Model(this._ogapi, this)
+    }
+
+    _composeElement() {
+        this._checkRequiredParameters()
+
+        var updateData = {
+            manufacturer: {
+                id: this._identifier || undefined,
+                name: this._name || undefined,
+                description: this._description || undefined,
+                telephone: this._telephone || undefined,
+                address: this._address || undefined,
+                fax: this._fax || undefined,
+                url: this._manufUrl || undefined,
+                notes: this._notes || undefined
+            }
+        };
+
+        return updateData;
+    }
+
+    _buildURL() {
+        var url = this._resource + "/" + this._identifier
+        return url;
+    }
+}
---
+++ src/manufacturers/Model.js
@@ -0,0 +1,170 @@
+'use strict';
+
+import BaseProvision from '../provision/BaseProvision';
+import Manufacturers from './Manufacturer';
+import ModelMedia from './ModelMedia';
+
+/**
+ * This is a base object that contains all you can do about Models.
+ */
+export default class Models extends BaseProvision {
+
+    /**     
+     * @param {InternalOpenGateAPI} Reference to the API object.
+     */
+    constructor(ogapi, manufacturer) {
+        super(ogapi, "/models", undefined, ['identifier', 'name', 'manufacturer']);
+
+        this._manufacturer = manufacturer
+    }
+
+    /**
+     * Set the identifier attribute
+     * @param {string} id - required field
+     * @return {Models}
+     */
+    withIdentifier(id) {
+        if (typeof id !== 'string' || id.length > 50)
+            throw new Error("OGAPI_STRING_PARAMETER_MAX_LENGTH_50");
+        this._identifier = id;
+        return this;
+    }
+    
+    /**
+     * Set the name attribute
+     * @param {string} name - required field
+     * @return {Models}
+     */
+    withName(name) {
+        if (typeof name !== 'string')
+            throw new Error("OGAPI_STRING_PARAMETER");
+        this._name = name;
+        return this;
+    }
+
+    /**
+     * Set the description attribute
+     * @param {string} description - required field
+     * @return {Models}
+     */
+    withDescription(description) {
+        if (typeof description !== 'string')
+            throw new Error("OGAPI_STRING_PARAMETER");
+        this._description = description;
+        return this;
+    }
+
+    /**
+     * Set the notes attribute
+     * @param {string} notes 
+     * @return {Models}
+     */
+    withNotes(notes) {
+        if (typeof notes !== 'string')
+           throw new Error("OGAPI_STRING_PARAMETER");
+        this._notes = notes;
+        return this;
+    }
+
+    /**
+     * Set the url attribute
+     * @param {string} url 
+     * @return {Models}
+     */
+    withUrl(url) {
+        if (typeof url !== 'string')
+           throw new Error("OGAPI_STRING_PARAMETER");
+        this._manufUrl = url;
+        return this;
+    }
+
+    /**
+     * Set the version attribute
+     * @param {string} version 
+     * @return {Models}
+     */
+    withVersion(version) {
+        if (typeof version !== 'string')
+           throw new Error("OGAPI_STRING_PARAMETER");
+        this._version = version;
+        return this;
+    }
+
+    /**
+     * Set the manufacturer attribute
+     * @param {Manufacturers} manufacturer 
+     * @return {Models}
+     */
+    withManufacturer(manufacturer) {
+        if (!manufacturer)
+           throw new Error("OGAPI_NOT_EMPTY_PARAMETER");
+        this._manufacturer = manufacturer;
+        return this;
+    }
+
+    /**
+     * Set the manufacturer identifier attribute
+     * @param {string} id
+     * @return {Manufacturers}
+     */
+    withManufacturerIdentifier(id) {
+        if (typeof id !== 'string' || id.length > 50)
+            throw new Error("OGAPI_STRING_PARAMETER_MAX_LENGTH_50");
+        if (!this._manufacturer) {
+            this._manufacturer = new Manufacturers(this._ogapi)
+        }
+        
+        this._manufacturer.withIdentifier(id);
+        return this;
+    }
+
+    /**
+     * Set the manufacturer name attribute
+     * @param {string} name
+     * @return {Models}
+     */
+    withManufacturerName(name) {
+        if (!name)
+            throw new Error("OGAPI_STRING_PARAMETER");
+        
+        if (!this._manufacturer) {
+            this._manufacturer = new Manufacturers(this._ogapi)
+        }
+        
+        this._manufacturer.withName(name);
+
+        return this;
+    }
+    
+
+    mediaBuilder() {
+        if (!this._identifier)
+            throw new Error("Required model identifier");
+        return new ModelMedia(this._ogapi, this._identifier)
+    }
+
+    _composeElement() {
+        this._checkRequiredParameters()
+
+        var updateData = {
+            model: {
+                id: this._identifier || undefined,
+                name: this._name || undefined,
+                description: this._description || undefined,
+                version: this._version || undefined,
+                notes: this._notes || undefined,
+                manufacturer: {
+                    id: this._manufacturer._identifier || undefined,
+                    name: this._manufacturer._name || undefined    
+                }
+            }
+        };
+
+        return updateData;
+    }
+
+    _buildURL() {
+        var url = this._resource + "/" + this._identifier
+        return url;
+    }
+}
