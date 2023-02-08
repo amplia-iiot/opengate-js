9c6dd21e705f44842e8f1b0ef5e05e6fda7c65d7
monicafernandez <monica.fernandez@amplia.es>
2023-02-08 17:10:49 +0100

    OUW-3924 (#82)
    * OUW-3924
    
    * change post for get

--- src/provision/country/CountriesCatalog.js
+++ src/provision/country/CountriesCatalog.js
@@ -1,22 +1,21 @@
 'use strict';
 
-import GenericFinder from '../../GenericFinder';
+import ProvisionGenericFinder from '../../ProvisionGenericFinder';
+import jp from 'jsonpath';
 import q from 'q';
 import HttpStatus from 'http-status-codes';
-
 /**
  *   This class allow make get request to countries catalog resource into Opengate North API.
+ *   //IMPORTANTE: ver tarea OUW-3924 para entender la realización de esta clase
  */
-export default class CountriesCatalog extends GenericFinder {
+export default class CountriesCatalog extends ProvisionGenericFinder {
 
     /**     
      * @param {InternalOpenGateAPI} ogapi - Reference to the API object.
      */
     constructor(ogapi) {
-        super(ogapi, 'countries/catalog', "countries", 'Countries not found');
-        // IMPORTANTE: cuando se implemente el catalogo de countries en API-REST, eliminar este código
-        this.__deleteCountriesFilter = ogapi.Napi._options._internalCountriesFilter
-        this.__deleteResource = 'search/entities'
+        super(ogapi, 'organizations', "entity", 'Countries not found');
+        this.__filter = this._api._options._internalCountriesFilter
     }
 
     /**
@@ -30,65 +29,42 @@ export default class CountriesCatalog extends GenericFinder {
     }
 
     _composeUrl() {
-        return this._baseUrl;
+        return this._baseUrl + "/" + this.__filter.organization + "/entities/" + this.__filter.identifier;
     }
 
-    // IMPORTANTE: cuando se implemente el catalogo de countries en API-REST, eliminar este método
-    __deleteFilter() {
-        let resourceType = this.__deleteCountriesFilter.resourceType
-        let filter = {
-            'and': [{
-                    'eq': {
-                        resourceType: resourceType
-                    }
-                },
-                {
-                    'eq': {
-                        'provision.administration.identifier': this.__deleteCountriesFilter.identifier
-                    }
-                }
-            ]
-        }
-        let specificTypeFilter = {}
-        specificTypeFilter['provision.' + resourceType.replace('entity.', '') + '.specificType._current.value'] = this.__deleteCountriesFilter.specificType
-        filter.and.push({ 'eq': specificTypeFilter })
-        return {
-            filter: filter,
-            limit: {
-                size: 1,
-                start: 1
-            }
-        }
-    }
-
-    // IMPORTANTE: cuando se implemente el catalogo de countries en API-REST, eliminar este método
+    /**
+     * @return {Promise}
+     * @private
+     */
     _execute() {
         let defered = q.defer();
         let promise = defered.promise;
-        this._api
-            .post(this.__deleteResource, this.__deleteFilter(), this._timeout, null, {flattened: true}, this._getServiceBaseURL())
-            .then((response) => {
-                let resultQuery = response.body;
-                let statusCode = response.statusCode;
-                let countries = []
-                if (statusCode === 204 || (statusCode === 200 && resultQuery['entities'].length === 0)) {
+        let _this = this;
+        let _error_not_found = this._error_not_found;
+        this._api.get(this._composeUrl(), undefined, this._getExtraHeaders(), this._getUrlParameters(), false, this._getServiceBaseURL())
+            .then((req) => {
+                if (req.statusCode === 204) {
                     defered.reject({
-                        error: this._error_not_found,
+                        error: _error_not_found,
                         statusCode: HttpStatus.NOT_FOUND
-                    })
+                    });
                 } else {
-                    let entity = resultQuery['entities'][0]
-                    countries = entity['provision.onfieldDeployments.fa.location'] && entity['provision.onfieldDeployments.fa.location']._value._current.value
+                    var data = ( jp.query(req.body, '$.' + _this.__filter.ds + '._current.value') || [])[0]
+                    defered.resolve({
+                        data: data,
+                        statusCode: req.statusCode
+                    });
+                    // }
                 }
-                defered.resolve({
-                    data: countries,
-                    statusCode: statusCode
-                });
             })
-
             .catch((error) => {
+                // BUG RELACIONADO (http://cm.amplia.es/jira/browse/OGODM-3250)
+                if (error.statusCode === 400) {
+                    error.statusCode = HttpStatus.NOT_FOUND;
+                }
                 defered.reject(error);
             });
         return promise;
     }
+
 }
