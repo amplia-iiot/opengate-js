54f37b681abdd1ea76409b20bb431fc985cf81df
Chema Perez <josemaria.perez@amplia.es>
2023-07-06 11:13:56 +0200

    OUW-4030

--- src/datasets/Datasets.js
+++ src/datasets/Datasets.js
@@ -17,6 +17,10 @@ export default class Datasets extends BaseProvision {
 
     _buildURL() {
         let url = 'datasets/provision/organizations/' + this._organization + '/' + this._identifier;
+
+        if (this._onlyPlan) {
+            url += '?onlyPlan=true'
+        }
         return url;
     }
 
@@ -75,16 +79,17 @@ export default class Datasets extends BaseProvision {
         this._description = description;
         return this;
     }
+
     /**
      * Set the columns attribute
      * @param {array} columns - required field
      * @return {Datasets}
      */
-  withColumns(columns) {
-    checkType._checkArray(columns, 'columns');
-    this._columns = columns;
-    return this;
-}
+    withColumns(columns) {
+        checkType._checkArray(columns, 'columns');
+        this._columns = columns;
+        return this;
+    }
     
 
     _composeElement() {
@@ -99,11 +104,27 @@ export default class Datasets extends BaseProvision {
         return dataset;
     }
 
+    _composeUpdateElement() {
+        let dataset = {
+            name: this._name,
+            description: this._description,
+            type: this._type,
+            identifierColumn: this._identifierColumn,
+            columns: this._columns
+        };
+        return dataset;
+    }
+
+    onlyPlan() {
+        this._onlyPlan = true;
+        return this;
+    }
+
     /**
      * This method invalidates the update option
      * @throws {Allways} Datasets cannot be updated.
      */
-     update() {
-        throw new Error("OGAPI_DAATASET_NOT_UPDATED");
-    }
+    //  update() {
+    //     throw new Error("OGAPI_DAATASET_NOT_UPDATED");
+    // }
 }
