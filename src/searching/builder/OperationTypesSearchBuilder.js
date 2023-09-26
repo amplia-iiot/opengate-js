ec8849dabea03a57b8900dd979f926f2b75f2dbf
monicafernandez <monica.fernandez@amplia.es>
2023-09-26 16:49:23 +0200

    OUW-4072

--- src/searching/builder/OperationTypesSearchBuilder.js
+++ src/searching/builder/OperationTypesSearchBuilder.js
@@ -2,7 +2,7 @@
 
 import SearchBuilder from './SearchBuilder';
 import FieldFinder from '../../util/searchingFields/FieldFinder';
-
+import SearchWithoutLimit from '../SearchWithoutLimit';
 const BASE_URL = '/operationTypes';
 /**
  * Defined a search over operationTypes	
@@ -15,5 +15,31 @@ export default class OperationTypesBuilder extends SearchBuilder {
     constructor(parent) {
         super(parent, {}, new FieldFinder(parent, BASE_URL));
         this._url = BASE_URL;
+    }
+
+
+    /**
+     * Build a instance of Search 
+     *
+     * @example
+     *  ogapi.devicesSearchBuilder().onProvisioned().build()
+     * @throws {SearchBuilderError} Throw error on url build
+     * @return {Search} 
+     */
+    build() {
+        try{
+            
+        return new SearchWithoutLimit(this._parent,
+            this._buildUrl(),
+            this._buildFilter(),
+            this._buildSort(),
+            this._buildGroup(),
+            this._buildSelect(),
+            this._builderParams.timeout,
+            this._urlParams);
+        }
+        catch(error){
+            console.error('!errrrror!!!!!!', error)
+        }
     }
 }
