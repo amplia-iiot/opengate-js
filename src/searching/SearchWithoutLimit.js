83dfee9f840857874481528e0640ffaba11bb712
monicafernandez <monica.fernandez@amplia.es>
2023-09-26 17:00:07 +0200

    OUW-4072 (#91)

---
+++ src/searching/SearchWithoutLimit.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import BaseSearch from './BaseSearch';
+import merge from 'merge';
+
+/** 
+ * This extends BaseSearch and allow make request to any available resource into Opengate North API.
+ */
+export default class SearchWithoutLimit extends BaseSearch {
+	/**
+    * @param {!InternalOpenGateAPI} ogapi - this is configuration about Opengate North API.
+    * @param {!string} url - this define a specific resource to make the search
+    * @param {object} filter - this is the filter
+    * @param {object} limit - this is the pagination about the search
+    * @param {object} sort - this defined parameters to order the result of search
+	* @param {object} group
+	* @param {object} select
+   	* @param {nubmer} timeout
+    */
+	constructor(ogapi, url, filter, sort, group, select, timeout, urlParams) {
+		super(ogapi, 'search' + url, timeout);
+		this._setUrlParameters(urlParams);
+		this._postObj = merge(filter, group, select);
+		if (typeof sort === 'object') {
+			this._postObj = merge(this._postObj, sort);
+		}
+	}
+
+	_filter() {
+		return this._postObj;
+	}
+
+}
