29f8f445650f7a842c7b6a1f457b929b46af17e4
jousepo <josemaria.perez@amplia.es>
2025-02-20 15:35:17 +0100

    OUW-4570

---
+++ src/notebookScheduler/SchedulerFinder.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import GenericFinder from '../GenericFinder';
+
+/**
+ *   This class allow make get request to planner resource into Opengate North API.
+ */
+export default class SchedulerFinder extends GenericFinder {
+
+    /**     
+     * @param {InternalOpenGateAPI} ogapi - Reference to the API object.
+     */
+    constructor(ogapi) {
+        super(ogapi, 'schedulers', 'list', 'Scheduler not found', 'planner');
+    }
+
+     /**
+     * Download a complete list of notebook schedulers for the user. This execute a GET http method
+     * @test
+     *   ogapi.newNotebooksSchedulerFinder().findAll().then().catch();
+     * @return {Promise} 
+     */
+    findAll() {
+        return this._execute();
+    }
+
+    /**
+     * @return {String} This returns a string with the URL of the request.
+     * @private
+     */
+    _composeUrl() {
+        return this._baseUrl;
+    }
+}
