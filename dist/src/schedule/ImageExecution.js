d9bbf9b6add7eb111aed15b3bd2a7d09ebd59646
jousepo <josemaria.perez@amplia.es>
2026-01-14 08:14:39 +0100

    OUW-4803

--- dist/src/schedule/ImageExecution.js
+++ dist/src/schedule/ImageExecution.js
@@ -83,32 +83,97 @@ var ImageExecution = (function (_BaseProvision) {
          */
     }, {
         key: 'withScheduleCronExpression',
-        value: function withScheduleCronExpression(cronExpression) {
+        value: function withScheduleCronExpression(cronExpression, timezone) {
             _utilFormatsCheck_types2['default']._checkString(cronExpression, 'cronExpression');
+            _utilFormatsCheck_types2['default']._checkString(timezone, 'timezone');
 
             if (!this._schedule) {
                 this._schedule = {};
             }
 
-            this._schedule.expression = cronExpression;
+            this._schedule = {
+                cron: {
+                    expression: cronExpression,
+                    timeZone: timezone
+                }
+            };
             return this;
         }
 
         /**
-         * Sets the isImmediateExecution attribute for schedule
-         * @param {boolean} isImmediateExecution
+         * Sets the interval for schedule in minutes
+         * @param {number} interval in minutes
          * @return {ImageExecution}
          */
     }, {
-        key: 'withScheduleImmediateExecution',
-        value: function withScheduleImmediateExecution(isImmediateExecution) {
-            _utilFormatsCheck_types2['default']._checkBoolean(isImmediateExecution, 'isImmediateExecution');
+        key: 'withScheduleMinutesInterval',
+        value: function withScheduleMinutesInterval(interval) {
+            _utilFormatsCheck_types2['default']._checkNumber(interval, 'interval');
 
             if (!this._schedule) {
                 this._schedule = {};
             }
 
-            this._schedule.isImmediateExecution = isImmediateExecution;
+            this._schedule.interval = {
+                minutes: interval
+            };
+
+            return this;
+        }
+
+        /**
+         * Sets the executeNow attribute
+         * @param {boolean} executeNow
+         * @return {ImageExecution}
+         */
+    }, {
+        key: 'withScheduleExecuteNow',
+        value: function withScheduleExecuteNow(executeNow) {
+            _utilFormatsCheck_types2['default']._checkBoolean(executeNow, 'executeNow');
+
+            if (!this._schedule) {
+                this._schedule = {};
+            }
+
+            this._schedule.executeNow = executeNow;
+
+            return this;
+        }
+
+        /**
+         * Sets the from attribute
+         * @param {string} from 
+         * @returns {ImageExecution}
+         */
+    }, {
+        key: 'withScheduleFrom',
+        value: function withScheduleFrom(from) {
+            _utilFormatsCheck_types2['default']._checkISODateTime(from, 'from');
+
+            if (!this._schedule) {
+                this._schedule = {};
+            }
+
+            this._schedule.from = from;
+
+            return this;
+        }
+
+        /**
+         * Sets the to attribute
+         * @param {string} to 
+         * @returns {ImageExecution}
+         */
+    }, {
+        key: 'withScheduleTo',
+        value: function withScheduleTo(to) {
+            _utilFormatsCheck_types2['default']._checkISODateTime(to, 'to');
+
+            if (!this._schedule) {
+                this._schedule = {};
+            }
+
+            this._schedule.to = to;
 
             return this;
         }
