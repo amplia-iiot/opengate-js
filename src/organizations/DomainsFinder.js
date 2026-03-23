242eaed61586a94e77181b57a2b73a2df1d8fdf8
jousepo <josemaria.perez@amplia.es>
2026-03-23 15:23:18 +0100

    PHO-4991

---
+++ src/organizations/DomainsFinder.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import ProvisionGenericFinder from '../ProvisionGenericFinder';
+
+/**
+ *   This class allow make get request to certificate resource into Opengate North API.
+ */
+export default class DomainFinder extends ProvisionGenericFinder {
+
+    /**
+     * Constructor
+     * @param {InternalOpenGateAPI} Reference to the API object.
+     */
+    constructor(ogapi) {
+        super(ogapi, 'domains', 'domain', 'Domain not found');
+    }
+
+    /**
+     * @return {String} This returns a string with the URL of the request.
+     * @private
+     */
+    _composeUrl() {
+        this._setUrlParameters({
+            hierarchy: this._hierarchy
+        });
+        return this._baseUrl + "/" + this._name;
+    }
+
+    /**
+     * Constructor
+     * @param {string} name - domain name.
+     * @return {Promise} 
+     */
+    findByNameWithHierarchy(name) {
+        this._name = name;
+        this._hierarchy = 'true';
+        return this._execute();
+    }
+
+}
